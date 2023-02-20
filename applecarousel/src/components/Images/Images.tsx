7f954c3e39f14da0e4bfbe4094cc5cb9ce71b981
brgndy <brgndy96@gmail.com>
2023-02-20 16:23:39 +0900

    msg

--- applecarousel/src/components/Images/Images.tsx
+++ applecarousel/src/components/Images/Images.tsx
@@ -1,13 +1,34 @@
 import classes from "./Images.module.css";
+const images = [
+  "https://via.placeholder.com/1000x400/0000FF/FFFFFF?text=Image+1",
+  "https://via.placeholder.com/1000x400/FF0000/FFFFFF?text=Image+2",
+  "https://via.placeholder.com/1000x400/00FF00/FFFFFF?text=Image+3",
+];
 
 export default function Images(props: any) {
   return (
-    <div className={classes.img_container}>
-      <img
-        className={`${classes.img} ${classes.prev}`}
-        src={props.url}
-        alt="img"
-      ></img>
-    </div>
+    <>
+      <div className={classes.img_container}>
+        <img
+          className={`${classes.img} ${classes.prev}`}
+          src={images[0]}
+          alt="img"
+        ></img>
+      </div>
+      <div className={classes.img_container}>
+        <img
+          className={`${classes.img} ${classes.active}`}
+          src={images[1]}
+          alt="img"
+        ></img>
+      </div>
+      <div className={classes.img_container}>
+        <img
+          className={`${classes.img} ${classes.next}`}
+          src={images[2]}
+          alt="img"
+        ></img>
+      </div>
+    </>
   );
 }
--- applecarousel/src/components/Slider.tsx
+++ applecarousel/src/components/Slider.tsx
@@ -2,18 +2,10 @@ import { useEffect, useState } from "react";
 import "./Slider.css";
 import Images from "./Images/Images";
 
-const images = [
-  "https://via.placeholder.com/1000x400/0000FF/FFFFFF?text=Image+1",
-  "https://via.placeholder.com/1000x400/FF0000/FFFFFF?text=Image+2",
-  "https://via.placeholder.com/1000x400/00FF00/FFFFFF?text=Image+3",
-];
-
 const Slider = () => {
   return (
     <div className="carousel-container">
-      {images.map((image, index) => {
-        return <Images url={image} key={index} />;
-      })}
+      <Images />
     </div>
   );
 };
