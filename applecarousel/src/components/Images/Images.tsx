766040240e19ce7b9c8411aa3e092773fe1c915c
brgndy <brgndy96@gmail.com>
2023-02-21 00:43:48 +0900

    msg

--- applecarousel/src/components/Images/Images.tsx
+++ applecarousel/src/components/Images/Images.tsx
@@ -1,5 +1,6 @@
 import classes from "./Images.module.css";
 import { useState, useEffect } from "react";
+
 const images = [
   "https://via.placeholder.com/1000x400/0000FF/FFFFFF?text=Image+1",
   "https://via.placeholder.com/1000x400/FF0000/FFFFFF?text=Image+2",
@@ -8,6 +9,8 @@ const images = [
 
 export default function Images(props: any) {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [prevIndex, setPrevIndex] = useState(images.length - 1);
+  const [nextIndex, setNextIndex] = useState(1);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -17,6 +20,11 @@ export default function Images(props: any) {
     return () => {
       clearInterval(interval);
     };
+  }, [activeIndex]);
+
+  useEffect(() => {
+    setPrevIndex(getPrevIndex(activeIndex));
+    setNextIndex(getNextIndex(activeIndex));
   }, [activeIndex]);
 
   const getPrevIndex = (index: number) => {
@@ -36,28 +44,28 @@ export default function Images(props: any) {
   };
 
   return (
-    <>
-      <div className={classes.img_container}>
-        <img
-          className={`${classes.img} ${classes.prev}`}
-          src={images[getPrevIndex(activeIndex)]}
-          alt="img"
-        ></img>
-      </div>
-      <div className={classes.img_container}>
-        <img
-          className={`${classes.img} ${classes.active}`}
-          src={images[activeIndex]}
-          alt="img"
-        ></img>
-      </div>
-      <div className={classes.img_container}>
-        <img
-          className={`${classes.img} ${classes.next}`}
-          src={images[getNextIndex(activeIndex)]}
-          alt="img"
-        ></img>
-      </div>
-    </>
+    <div className={classes.img_container}>
+      <img
+        className={`${classes.img} ${classes.prev} ${
+          activeIndex > prevIndex ? classes.prev_out : classes.prev_in
+        }`}
+        src={images[prevIndex]}
+        alt="img"
+      ></img>
+      <img
+        className={`${classes.img} ${classes.active} ${
+          activeIndex > prevIndex ? classes.active_right : classes.active_left
+        }`}
+        src={images[activeIndex]}
+        alt="img"
+      ></img>
+      <img
+        className={`${classes.img} ${classes.next} ${
+          activeIndex > prevIndex ? classes.next_in : classes.next_out
+        }`}
+        src={images[nextIndex]}
+        alt="img"
+      ></img>
+    </div>
   );
 }
